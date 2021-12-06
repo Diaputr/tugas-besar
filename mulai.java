0e5bbe66de5b804ec3749159f19eae34e7452d02
cαεsαɾ <fikrilha099@gmail.com>
2021-12-06 09:33:59 +0700

    perbaikan error

--- mulai.java
+++ mulai.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
-public class mulai{
+
+public class mulai {
   public static void main(String[] args){
     Scanner senin = new Scanner(System.in);
-    System.out.print("Masukkan jenis barang: ";
+    System.out.print("Masukkan jenis barang: ");
 		System.out.print("Masukkan berat barang: "+"kg");
     int input = sc.nextInt();
   }
