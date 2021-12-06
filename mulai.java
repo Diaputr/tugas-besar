3a8c4ac5dbd2425d75ed59cfbab0070f0a9c8e40
cαεsαɾ <fikrilha099@gmail.com>
2021-12-06 10:30:40 +0700

    penambahan fungsi

--- mulai.java
+++ mulai.java
@@ -1,7 +1,28 @@
+import java.io.PrintStream;
 import java.util.Scanner;
+
 public class mulai {
+  public static void judul() {
+    System.out.println("         SELAMAT DATANG           ");
+    System.out.println("----------------------------------");
+    System.out.println("      ~PAKET JATIM SENTOSA~       ");
+    System.out.println("SIAP MELAYANI SAMPAI GULUNG TIKAR ");
+  }
+
+  public static void jenisBarang() {
+    System.out.println("\n\nPilih jenis barang:");
+    System.out.println("1. Elektronik");
+    System.out.println("2. Farmasi");
+    System.out.println("3. Apparel");
+    System.out.println("4. Makanan dan Minuman");
+    System.out.println("5. Kosmetik");
+    System.out.println("6. Otomotif");
+  }
+
   public static void main(String[] args){
     Scanner input = new Scanner(System.in);
+    judul();
+    jenisBarang();
     System.out.print("Masukkan jenis barang: ");
     System.out.print("Masukkan berat barang: "+"kg");
     int input = sc.nextInt();
