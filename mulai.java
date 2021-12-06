a38d362251d410c15bc8d1d883787bdeca5e42e1
Lailatul Badriyah <90756740+lailatulbadriyah24@users.noreply.github.com>
2021-12-06 09:35:42 +0700

    Update mulai.java

--- mulai.java
+++ mulai.java
@@ -4,7 +4,7 @@ public class mulai {
   public static void main(String[] args){
     Scanner senin = new Scanner(System.in);
     System.out.print("Masukkan jenis barang: ");
-		System.out.print("Masukkan berat barang: "+"kg");
+    System.out.print("Masukkan berat barang: "+"kg");
     int input = sc.nextInt();
   }
 }
