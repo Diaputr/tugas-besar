4c49237282b49ee177b809e8296b35297a709622
cαεsαɾ <fikrilha099@gmail.com>
2021-12-06 10:53:23 +0700

    penambahan perulangan

--- mulai.java
+++ mulai.java
@@ -19,38 +19,42 @@ public class mulai {
     System.out.println("6. Otomotif");
   }
 
-  public static void main(String[] args){
-    Scanner input = new Scanner(System.in);
-    judul();
-    jenisBarang();
-    System.out.print("Masukkan jenis barang: ");
-    System.out.print("Masukkan berat barang: "+"kg");
-    int input = sc.nextInt();
-    
-    System.out.println("==================== Program Ekspedisi ====================");
-    System.out.println("=================== Provinsi Jawa Timur ===================");
-    System.out.println("Jenis Ekspedisi ");
-    System.out.println("[1] -> Paket Reguler");
-    System.out.println("[2] -> Paket Kilat");
-    System.out.println("[3] -> Paket Kontainer");
-    System.out.print("Masukkan Pilihan Anda: ");
-    int input = sc.nextInt();
-            int tarifPerkilo = 0;
-        switch (input) {
-            case (1) :
-                tarifPerkilo = 5000;
-            break;
-            case (2) :
-                tarifPerkilo = 2000;
-            break;
-            case (3) :
-                tarifPerkilo = 3000;
-            break;
-            default :
-		System.out.println("Jenis Packaging ");
-    System.out.println("[1] -> Plastik");
-    System.out.println("[2] -> Kardus);
-    System.out.print("Masukkan Pilihan Anda: ");
-    int input = sc.nextInt();
-  }
-}
+  public static void main(String[] args) {
+      Scanner scInt = new Scanner(System.in);
+      judul();
+      jenisBarang();
+      System.out.print("Masukkan jenis barang: ");
+      byte jenisBarang = scInt.nextByte();
+      System.out.print("Masukan jumlah barang: ");
+      byte jumlahBarang = scInt.nextByte();
+      float[] berat = new float[jumlahBarang];
+      for (int i = 0; i < berat.length; ++i) {
+        System.out.print("Masukkan berat barang: " + "kg");
+        berat[i] = scInt.nextFloat();
+      }
+      System.out.println("==================== Program Ekspedisi ====================");
+      System.out.println("=================== Provinsi Jawa Timur ===================");
+      System.out.println("Jenis Ekspedisi ");
+      System.out.println("[1] -> Paket Reguler");
+      System.out.println("[2] -> Paket Kilat");
+      System.out.println("[3] -> Paket Kontainer");
+      System.out.print("Masukkan Pilihan Anda: ");
+      byte jenis = scInt.nextByte();
+      int tarifPerkilo = 0;
+      switch (jenis) {
+        case (1):
+          tarifPerkilo = 5000;
+          break;
+        case (2):
+          tarifPerkilo = 2000;
+          break;
+        case (3):
+          tarifPerkilo = 3000;
+          break;
+        default:
+          System.out.println("Jenis Packaging ");
+          System.out.println("[1] -> Plastik");
+          System.out.println("[2] -> Kardus);
+            System.out.print("Masukkan Pilihan Anda: "); int input = sc.nextInt();
+          }
+      }
