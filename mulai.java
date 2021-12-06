bb8df4a43befebfb0db96c81ee579c6d9011be9f
Lailatul Badriyah <lailatulbadriyah654@gmail.com>
2021-12-06 09:33:13 +0700

    nyoba

--- mulai.java
+++ mulai.java
@@ -2,5 +2,8 @@ import java.util.Scanner;
 public class mulai{
   public static void main(String[] args){
     Scanner senin = new Scanner(System.in);
+    System.out.print("Masukkan jenis barang: ";
+		System.out.print("Masukkan berat barang: "+"kg");
+    int input = sc.nextInt();
   }
 }
