ab98c79db5da57846ba4f077ffb4aa0a22671df2
Diah Putri Nofianti <90998386+Diaputr@users.noreply.github.com>
2021-12-08 14:56:55 +0700

    -

--- fiturBarang.java
+++ fiturBarang.java
@@ -1,8 +1,24 @@
 import java.util.Scanner;
 public class fiturBarang{
 
-	static String[] jenisBarang = {"Buku/Dokumen", "Elektronik/Gadget", "Farmasi", "Kosmetik", 
-				"Makanan/Minuman", "Pakaian", "Pecah Belah", "Otomotif", "Dll"};
+	static String[] jenisBarang = {"Elektronik/Gadget", "Farmasi/Kosmetik", "Makanan/Minuman", "Pakaian", "Pecah Belah"};
+
+	static double barang(double berat, double jml){
+		int[] harga = {5000, 10000, 15000};
+		double bayar = 0;
+		if(berat>=0 && berat<5){
+			bayar=jml*berat*harga[0];
+			
+		} else if(berat>=5 && berat<10){
+			bayar=jml*berat*harga[1];
+			
+		} else if(berat>=10){
+			bayar=jml*berat*harga[2];
+		} else {
+			System.out.println("Error");
+		}
+		return bayar;
+	}
 
 	static void tampilBarang(){
 	  System.out.println("\n---Informasi Barang---\n");
@@ -10,54 +26,30 @@ public class fiturBarang{
       System.out.println((i+1) + ". " + jenisBarang[i]);
     }
   }
-	static double hitungBerat(double g, double h){
-	  double harga=0;
-	  if (g<=5){
-		  harga=5000*h;  
-	} else {
-		harga=8000*h;
-	}
-	return harga;
-  }
   
 	public static void main(String[] args) {
 	Scanner input = new Scanner(System.in);
 	
-	System.out.print("\nJumlah paket yang akan diantar : ");
-	int a = input.nextInt();
-	
-	for (int i=0; i<a; i++) {
-		if (a>1){
-		System.out.print("\n*PAKET KE-"+(i+1));
-		}
-		
      	tampilBarang();
 	
 	System.out.print("\nPilih jenis barang : ");
-	int b = input.nextInt();
-	if (b==9){
-		input.nextLine();
-		System.out.print("\nKetikkan jenis barang : ");
-		String dll = input.nextLine();
-	} else {
-		input.nextLine();
-	}
+	int jenis = input.nextInt();
 	
+	input.nextLine();
 	System.out.print("\nNama barang : ");
-	String c = input.nextLine();
+	String nama = input.nextLine();
 	
 	System.out.print("\nBerat barang (kg) : ");
-	double d = input.nextDouble();
+	double berat = input.nextDouble();
 	
 	System.out.print("\nJumlah barang : ");
-	double e = input.nextInt();
+	double jml = input.nextInt();
 	
 	input.nextLine();
 	System.out.print("\nTambah catatan/keterangan : ");
-	String f = input.nextLine();
+	String ket = input.nextLine();
 	
-	System.out.println(hitungBerat(d,e));
-	
-	}
+	System.out.println("Harga menurut berat barang : Rp. "+barang(berat, jml));
+
   }
 }
